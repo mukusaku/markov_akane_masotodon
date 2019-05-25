c543cd28161b69105fe5cda443183b8b2a4994a7
mukusaku <mukusakuproducer@gmail.com>
2019-05-25 14:47:58 +0900

    formatter.phpのファイルが間違っていたため差し替え

--- formatter.php
+++ formatter.php
@@ -26,17 +26,14 @@ $text = convertToAko($string);
 
 $markovText = $mc->makeMarkovText($text);
 
-echo $text."\n";
-echo '↓'."\n";
-echo $markovText."\n";
-echo '↓'."\n";
+//echo $text."\n";
+//echo '↓'."\n";
+//echo $markovText."\n";
+//echo '↓'."\n";
 
-//echo $a;
-// -> 楽天アクセスかきくけこ
 function convertToAko($sentence){
     // 変換対象の用語リストを配列で取得
-//    $aryConvertList = array();
-    $aryConvertList = array('アテクシ'=>'あかねちゃん','俺'=>'あかねちゃん');
+    $aryConvertList = array('アテクシ'=>'あかねちゃん','俺'=>'あかねちゃん'); // TODO 変換処理は別途実装する
     foreach($aryConvertList as $sBefore => $sAfter) {
         $sentence = str_replace($sBefore, $sAfter, $sentence);
     }
@@ -46,6 +43,27 @@ function convertToAko($sentence){
 
 ///////////////////////////////////////////
 
-$toot = substr($markovText,0,strpos($markovText, '。'));
-$command = `ruby /home/mastodon/akanebot_mastodon/bot.rb $toot`;
-echo $command;
+/* Settings */
+$schema       = 'https';
+$host         = 'akanechan.love';
+$access_token = '3b2822ca6af1899b8bd1a1dd89924d7adafc4d4323820f89543a1171f91dfc93';
+$method       = 'POST';
+$endpoint     = '/api/v1/statuses';
+$url          = "${schema}://${host}${endpoint}";
+$visibility   = 'unlisted'; //投稿のプライバシー設定→「未収載」
+$toot_msg     = substr($markovText,0,strpos($markovText, '。'));
+$toot_msg     = rawurlencode($toot_msg); //メッセージをcURL用にエスケープ
+
+/* Build request */
+$query  = "curl -X ${method}";
+$query .= " -d 'status=${toot_msg}'";
+$query .= " -d 'visibility=${visibility}'";
+$query .= " --header 'Authorization:";
+$query .= " Bearer ${access_token}'";
+$query .= " -sS ${url}";
+
+/* Request */
+$result = `$query`; //バッククォートに注意
+
+/* Show result */
+print_r(json_decode($result, JSON_OBJECT_AS_ARRAY));
