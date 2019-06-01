107ed5ef79753fe391df498feb65e47015d8fd4c
mukusaku <mukusakuproducer@gmail.com>
2019-06-01 17:44:38 +0900

    ver2.1.0
    HTLをふぁぼるようになった

--- exec_akane_toot.sh
+++ exec_akane_toot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/mastodon/akanebot_mastodon/
 php /home/mastodon/akanebot_mastodon/formatter.php
-php /home/mastodon/akanebot_mastodon/favoritereply.php
+php /home/mastodon/akanebot_mastodon/favorite.php
--- favorite.php
+++ favorite.php
@@ -3,13 +3,15 @@ require __DIR__ . '/vendor/autoload.php';
 require 'convertEntity.php';
 require 'originalList.php';
 use YuzuruS\Mecab\Markovchain;
-$toot = new main();
+$toot = new favorite();
 $toot->execFav();
 //$toot->execToot();
-class main {
+class favorite {
     function execFav(){
-        $aryInfo = $this->getNotifications();
-        $this->actionFavorite($aryInfo);
+        $aryNotifications = $this->getNotifications();
+        $aryTimeline = $this->getHomeTimeline();
+        $aryTarget = $aryNotifications + $aryTimeline;
+        $this->actionFavorite($aryTarget);
     }
 
     function getNotifications() {
@@ -41,6 +43,41 @@ class main {
             }
 //            print_r($value, false);
             $aryBt += array($value['status']['id'] => str_replace("@akane", "", strip_tags($value['status']['content'])));
+        }
+        print_r($aryBt, false);
+        return $aryBt;
+    }
+
+    function getHomeTimeline() {
+        // サーバ情報などの読み込み
+        $arySetting = parse_ini_file("mastodon_setting.ini");
+        /* Settings */
+        $schema       = 'https';
+        $host         = $arySetting['server'];
+        $access_token = $arySetting['access_token'];
+        $method       = 'GET';
+        $endpoint     = '/api/v1/timelines/home';
+        $url          = "${schema}://${host}${endpoint}";
+        $url         .= "?limit=15";
+        /* Build request */
+        $query  = "curl -X ${method}";
+        $query .= " --header 'Authorization:";
+        $query .= " Bearer ${access_token}'";
+        $query .= " -sS ${url}";
+        /* Request */
+        $result = `$query`; //バッククォートに注意
+        /* Show result */
+        $aryResult = json_decode($result, JSON_OBJECT_AS_ARRAY);
+        //print_r(json_decode($result, JSON_OBJECT_AS_ARRAY));
+
+        $aryBt = array();
+        foreach($aryResult as $key => $value) {
+            if($value['account']['username'] == "akane"
+                || strpos($value['content'], 'あかね') === false ) {
+                continue;
+            }
+//            print_r($value, false);
+            $aryBt += array($value['id'] => str_replace("@akane", "", strip_tags($value['content'])));
         }
         print_r($aryBt, false);
         return $aryBt;
