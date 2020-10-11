a5d7757ae8c60d06665d1b1011e1c668fd1f1bd1
mukusaku <mukusakuproducer@gmail.com>
2020-10-11 14:41:46 +0900

    サーバ接続用共通クラス、トゥート用クラスの実装

---
+++ mastodon/postActions/PostTootApi.php
@@ -0,0 +1,17 @@
+<?php
+namespace postActions;
+require __DIR__ . '/mastodon/ConnectionSettingsUtil.php';
+
+class PostTootApi {
+
+    public function toot($sentence, $bAdding = true) {
+        // 接頭辞、接尾辞の追加
+        if($bAdding) {
+            //$sentence = $this->addPrefix($sentence);
+            //$sentence = $this->addSuffix($sentence);
+        }
+
+        $connectionSettingsUtil = new ConnectionSettingsUtil();
+        $request = $connectionSettingsUtil->execToot($sentence);
+    }
+}
